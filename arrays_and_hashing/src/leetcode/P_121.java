af6322c804e3f955765a888db624d2f0734451b6
s.tulkunov <sarvartulkunov2002@gmail.com>
2025-01-08 09:12:08 +0500

    Leetcode: P121 solution

---
+++ arrays_and_hashing/src/leetcode/P_121.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+public class P_121 {
+
+    /**
+     * Approach: sliding window
+     * Input: prices = [10,1,5,6,7,1]
+     * Output: 6
+     */
+    public static int maxProfit(int[] prices) {
+
+        int l = 0, r = 1;
+        int maxProfit = 0;
+
+        while (r < prices.length) {
+            if (prices[l] < prices[r]) {
+                int profit = prices[r] - prices[l];
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                l = r;
+            }
+            r++;
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+//        System.out.println(maxProfit(new int[]{10, 1, 5, 6, 7, 1}));
+//        System.out.println(maxProfit(new int[]{10, 8, 7, 5, 2}));
+    }
+}
