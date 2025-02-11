1d7487dc4b43c73ea89714d68c732d7ab5e58df2
s.tulkunov <sarvartulkunov2002@gmail.com>
2025-02-11 19:50:47 +0500

    Leetcode P1910 solution

---
+++ arrays_and_hashing/src/leetcode/P_1910.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+/**
+ * 1910. Remove All Occurrences of a Substring
+ */
+public class P_1910 {
+
+    // First Solution
+    public static String removeOccurrences(String s, String part) {
+        while (s.contains(part)) {
+            s = s.replaceFirst(part, "");
+        }
+        return s;
+    }
+
+    /* Optimized Solution
+    public static String removeOccurrences(String s, String part) {
+        char[] input = s.toCharArray();
+        char[] target = part.toCharArray();
+        char[] resultStack = new char[input.length];
+        int stackSize = 0, targetLength = target.length;
+        char targetEndChar = target[targetLength - 1];
+
+        for (char currentChar : input) {
+            resultStack[stackSize++] = currentChar;
+
+            if (currentChar == targetEndChar && stackSize >= targetLength) {
+                int i = stackSize - 1, j = targetLength - 1;
+
+                while (j >= 0 && resultStack[i] == target[j]) {
+                    i--;
+                    j--;
+                }
+
+                if (j < 0) {
+                    stackSize = i + 1;
+                }
+            }
+        }
+        return new String(resultStack, 0, stackSize);
+    }
+    */
+
+    public static void main(String[] args) {
+        System.out.println(removeOccurrences("daabcbaabcbc", "abc"));
+        System.out.println(removeOccurrences("aabababa", "aba"));
+    }
+}
