126a814d691d9da824eb4ba2ad0b68ea4de8f215
s.tulkunov <s.tulkunov@yt.uz>
2025-03-11 17:16:36 +0500

    Leetcode P560 solution

---
+++ arrays_and_hashing/src/leetcode/P_560.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 560. Subarray Sum Equals K
+ */
+public class P_560 {
+
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0, sum = 0;
+        map.put(0, 1);
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
