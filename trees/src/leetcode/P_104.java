ad94c4fbe5cd5000487ccc1f80a553cb9513aef5
SarvarTulkunov <sarvartulkunov2002@gmail.com>
2025-08-10 10:10:17 +0500

    Leetcode Problem 104 and 543 solution

---
+++ trees/src/leetcode/P_104.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * 104. Maximum Depth of Binary Tree
+ */
+public class P_104 {
+
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
