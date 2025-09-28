e4c893f145314b76b54ef9df818c90cabc72b0b0
SarvarTulkunov <sarvartulkunov2002@gmail.com>
2025-09-28 21:35:42 +0300

    Leetcode P120 solution

---
+++ dynamic_programming/src/leetcode/P_120.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * 120. Triangle
+ */
+public class P_120 {
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+        int n = triangle.size();
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                triangle
+                        .get(i)
+                        .set(j, triangle.get(i).get(j) +
+                                Math.min(triangle.get(i + 1).get(j),
+                                        triangle.get(i + 1).get(j + 1)));
+            }
+        }
+
+        return triangle.get(0).get(0);
+    }
+}
